178554986fd70db63e9047a4c11e655637a44bf4
Alan <alan.profissional@outlook.com>
2026-05-25 23:46:40 -0300

    feat: add basic logging system to FastAPI app

--- app/main.py
+++ app/main.py
@@ -1,11 +1,25 @@
+import logging, os
 from fastapi import FastAPI
+
+os.makedirs("logs", exist_ok=True)
+
+logging.basicConfig(
+    filename=os.path.join("logs", "app.log"),
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s",
+)
 
 app = FastAPI()
 
+
 @app.get("/")
 def home():
+
+    logging.info(f"Rota: /")
     return {"message": "Hello World"}
+
 
 @app.get("/status")
 def status():
+    logging.info("Rota: Status")
     return {"status": "online"}
